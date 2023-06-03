594350bddf494c52a42c51fb459be89601b4b7e2
Yashil Vora <yashil2004@gmail.com>
2023-06-02 17:20:07 -0700

    model code added

---
+++ src/model.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from transformers import AutoModel
+
+
+class ClassifierHead(nn.Module):
+    def __init__(self, hidden_size, num_classes, seq_length):
+        super().__init__()
+        self.rnn = nn.RNN(768, hidden_size, batch_first=True)
+        self.linear = nn.Linear(hidden_size*seq_length, num_classes)
+
+    def forward(self, x):
+        x, _ = self.rnn(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.linear(x)
+        return x
+
+class BERTWithClassifierHead(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = AutoModel.from_pretrained("bert-base-uncased")
+        # ClassifierHead is already defined.
+        self.classifier_head = ClassifierHead(20, num_classes, 500)
+        
+    def forward(self, x):
+        x = self.backbone(**x)
+        x = x.last_hidden_state
+        x = self.classifier_head(x)
+        return x
