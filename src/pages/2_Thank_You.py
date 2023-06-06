92c9ebd1785430d258de533e2684d1d2ad806bfa
Kevin S <firstkevinshen@gmail.com>
2023-06-05 22:42:25 -0700

    Small Changes

--- src/pages/2_Thank_You.py
+++ src/pages/2_Thank_You.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="Aknowledgements")
-st.sidebar.header("Aknowledgements")
+st.set_page_config(page_title="Acknowledgements")
+st.sidebar.header("Acknowledgements")
 
 st.markdown(
     """
     ## Thank you!
 
     We would like to thank ACM AI for giving us the oppurtunity to build this project.
-    We would also like to knowledge and thank our team mentor Vincent who helped us 
+    We would also like to acknowledge and thank our team mentor Vincent who helped us 
     throughout the project and constantly pushed us to meet deadlines and complete all
     tasks. 
     
